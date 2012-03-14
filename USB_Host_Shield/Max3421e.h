be7215e0217eaaa15e76fa141cd7dc6bdcc6b187
Yuuichi Akagawa <yuuichi.akagawa@gmail.com>
2012-03-15 01:25:43 +0900

    Unite USB_Host_Shield codes

--- USB_Host_Shield/Max3421e.h
+++ USB_Host_Shield/Max3421e.h
@@ -1,14 +1,109 @@
-//*********************
-//
-//Kenichi Yoshida
-//2012/01/31
-//
-//********************
+/*
+  Copyright 2012 ADK Study Group Tokyo
 
-#if defined(__AVR_ATmega1280__) || (__AVR_ATmega2560__)
-	#include "demokit/Max3421e.h"
-#endif
+  Licensed under the Apache License, Version 2.0 (the "License");
+  you may not use this file except in compliance with the License.
+  You may obtain a copy of the License at
 
-#if  defined(__AVR_ATmega168__) || defined(__AVR_ATmega328P__)
-	#include "circuit/Max3421e.h"
+    http://www.apache.org/licenses/LICENSE-2.0
+
+  Unless required by applicable law or agreed to in writing, software
+  distributed under the License is distributed on an "AS IS" BASIS,
+  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+  See the License for the specific language governing permissions and
+  limitations under the License.
+
+  Changes: 
+    Fix for the Arduino IDE version compatibility.
+    Supoort ADK boards
+      Google ADK compatible boards(RT-ADK, Arduino MegaADK)
+      Duemilanove/UNO + Sparkfun USB Host Shield(DEV-09947)
+      Pro mini + USB Host Shield for Arduino Pro Mini
+
+ -----------------------------------------------------------------------------------
+ * Copyright 2009-2011 Oleg Mazurov, Circuits At Home, http://www.circuitsathome.com
+ * MAX3421E USB host controller support
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ * 3. Neither the name of the authors nor the names of its contributors
+ *    may be used to endorse or promote products derived from this software
+ *    without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+ * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+ * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ */
+
+/* MAX3421E functions */
+#ifndef _MAX3421E_H_
+#define _MAX3421E_H_
+
+
+//#include <Spi.h>
+//#include <WProgram.h>
+#if defined(ARDUINO) && ARDUINO >= 100
+#include "Arduino.h"
+#else
+#include "WProgram.h"
 #endif
+#include "Max3421e_constants.h"
+
+class MAX3421E /* : public SPI */ {
+    // byte vbusState;
+    public:
+        MAX3421E( void );
+        byte getVbusState( void );
+//        void toggle( byte pin );
+        static void regWr( byte, byte );
+        char * bytesWr( byte, byte, char * );
+        static void gpioWr( byte );
+        byte regRd( byte );
+        char * bytesRd( byte, byte, char * );
+        byte gpioRd( void );
+        boolean reset();
+        boolean vbusPwr ( boolean );
+        void busprobe( void );
+        void powerOn();
+        byte IntHandler();
+        byte GpxHandler();
+        byte Task();
+    private:
+	static void pinInit(void);
+
+      static void spi_init() {
+        uint8_t tmp;
+        // initialize SPI pins
+        pinMode(SCK_PIN, OUTPUT);
+        pinMode(MOSI_PIN, OUTPUT);
+        pinMode(MISO_PIN, INPUT);
+        pinMode(SS_PIN, OUTPUT);
+        /* mode 00 (CPOL=0, CPHA=0) master, fclk/2. Mode 11 (CPOL=11, CPHA=11) is also supported by MAX3421E */
+        SPCR = 0x50;
+        SPSR = 0x01;
+        /**/
+        tmp = SPSR;
+        tmp = SPDR;
+    }
+//        void init();
+    friend class Max_LCD;        
+};
+
+
+
+
+#endif //_MAX3421E_H_
